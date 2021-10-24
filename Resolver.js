a9873bddb433dd7fe6fadab835f7fcf850e0aaeb
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-10-24 20:05:03 -0300

    feat: resolving scope
    Resolving the another part of the scope

--- Resolver.js
+++ Resolver.js
@@ -1,3 +1,5 @@
+const Lox = require('./lox');
+
 class Resolver {
   constructor(interpreter) {
     this.interpreter = interpreter;
@@ -13,12 +15,25 @@ class Resolver {
     this.visitVarStmt = this.visitVarStmt.bind(this);
     this.declare = this.declare.bind(this);
     this.define = this.define.bind(this);
+    this.visitVariableExpr = this.visitVariableExpr.bind(this);
+    this.resolveLocal = this.resolveLocal.bind(this);
+    this.visitAssignExpr = this.visitAssignExpr.bind(this);
+    this.visitFnStmt = this.visitFnStmt.bind(this);
+    this.resolveFn = this.resolveFn.bind(this);
   }
 
   visitBlockStmt(stmt) {
     this.beginScope();
     this.resolve(stmt.statements);
     this.endScope();
+    return null;
+  }
+
+  visitFnStmt(stmt) {
+    this.declare(stmt.name);
+    this.define(stmt.name);
+
+    this.resolveFn(stmtm);
     return null;
   }
 
@@ -31,10 +46,37 @@ class Resolver {
     return null;
   }
 
+  visitAssignExpr(expr) {
+    this.resolve(expr.value);
+    this.resolveLocal(expr, expr.name);
+    return null;
+  }
+
+  visitVariableExpr(expr) {
+    if (this.scopes.length > 0 &&
+        this.scopes[0].get(expr.name.lexeme) === false) {
+      Lox.error(expr.name,
+        'Can\'t read local variable in its own initializer.');
+    }
+
+    this.resolveLocal(expr, expr.name);
+    return null;
+  }
+
   resolve(statements) {
     for (let statement of statements) {
       this.resolveStmt(statement);
     }
+  }
+
+  resolveFn(fn) {
+    this.beginScope();
+    for (let param of fn.params) {
+      this.declare(param);
+      this.define(param);
+    }
+    this.resolve(fn.body);
+    this.endScope();
   }
 
   beginScope() {
@@ -55,6 +97,15 @@ class Resolver {
   define(name) {
     if (this.scopes.length === 0) return;
     this.scopes[0].set(name.lexeme, true);
+  }
+
+  resolveLocal(expr, name) {
+    for (let i = this.scopes.length - 1; i >= 0; i--) {
+      if (this.scopes[i].has(name.lexeme)) {
+        this.interpreter.resolve(expr, this.scopes.length - 1 - i);
+        return;
+      }
+    }
   }
 
   resolveStmt(stmt) {
